120248356abd221947985f251d7818f96d15c7c4
Hammer-01 <98681433+Hammer-01@users.noreply.github.com>
2024-06-07 01:23:17 +1000

    Simplify word count code

--- texteditor.js
+++ texteditor.js
@@ -6,6 +6,6 @@ let lineCount = document.getElementById('line-count');
 textBox.addEventListener('input', () => {
     let text = textBox.value;
     charCount.textContent = text.length;
-    wordCount.textContent = text.trim() ? text.trim().replace(/\s+/g, ' ').split(' ').length : 0;
+    wordCount.textContent = text.trim() ? text.trim().split(/\s+/).length : 0;
     lineCount.textContent = text ? text.split('\n').length : 0;
 });
